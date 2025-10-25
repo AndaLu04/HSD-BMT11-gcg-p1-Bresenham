b8cf533b85a6286162887b2804c3439258b0280f
AndaLu04 <luiserbert@outlook.de>
2025-10-25 15:59:47 +0200

    zwei for schleifen

--- bresenham.js
+++ bresenham.js
@@ -24,68 +24,74 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-function drawLine(_x0, _y0, _x1, _y1){
+function drawLine(x0, y0, x1, y1){
 
-  let x0 = _x0;
-  let y0 = _y0;
-  let x1 = _x1;
-  let y1 = _y1;
-
-  let dx = x1-x0;       //delta x
-  let dy = y1-y0;       //delta y
-
-  let m = dy / dx;    //Steigung
+    //Vorberechnungen für x
+    let dx = x1-x0;     //delta x
+    let betrag_dx = Math.abs(dx);
 
 
-  //Vorberechnungen für x
-
-  // Standard: Linie nach rechts, (dx>0)
-  let x_start = x0;
-  let x_end = x1+1; // damit bei x!=x_end keine Lücke entsteht
-  let x_step = 1;
+    // Standard: Linie nach rechts, (dx>0)
+    let x_start = x0;
+    let x_end = x1+1; // damit bei x!=x_end keine Lücke entsteht
+    let x_step = 1;
 
 
-  // Wenn Linie nach links, (dx<0)
-  if (dx<0) {
-      x_step = -1;    // wir wollen x dekrementieren um nach links zu laufen
-      x_end = x1-1;   // damit bei x!=x_end keine Lücke entsteht
-      dx = -dx;       // hiermit spiegeln wir an der y-Achse
-  }
+    // Wenn Linie nach links, (dx<0)
+    if (dx<0) {
+        x_step = -1;    // wir wollen x dekrementieren um nach links zu laufen
+        x_end = x1-1;   // damit bei x!=x_end keine Lücke entsteht
+        dx = -dx;       // hiermit spiegeln wir an der y-Achse
+    }
 
 
-  //Vorberechnungen für y
-  let y = y0;        
+    //Vorberechnungen für y
+    let y = y0;        
+    let dy = y1-y0;      //delta y
+    let betrag_dy = Math.abs(dy);
 
 
-  let y_step = 1;       
+    let y_step = 1;       
 
-  if (dy<0) {
-      y_step = -1;
-      dy = -dy;       // spiegelung an der x-Achse       
-  }
+    if (dy<0) {
+        y_step = -1;
+        dy = -dy;       // Spiegelung an der x-Achse       
+    }
 
-  let Q = 2*dy-dx;
-  let Q_step = 2*(dy-dx);
-  let Q_equal = 2*dy;
+    let Q = 2*dy-dx;
+    let Q_step = 2*(dy-dx);
+    let Q_equal = 2*dy;
 
-    let x;
-  if (m<1){
-    let temp = x;
-    x = y;
-    y = temp
-  }
-  // x_start: startpunkt; 
-  // x!=x_end: wir machen so lange weiter, wie das aktuelle x ungleich x_end ist; 
-  // x_step: entscheidet über Inkrement oder Dekrement.
-  for (x=x_start; x!=x_end; x=x+x_step) {
-      setPixel(x, y);
-      if (Q < 0){
-          Q=Q+Q_equal;
+    // x_start: startpunkt; 
+    // x!=x_end: wir machen so lange weiter, wie das aktuelle x ungleich x_end ist; 
+    // x_step: entscheidet über Inkrement oder Dekrement.
 
-      } else {
-          Q=Q+Q_step;
-          y=y+y_step;
+    if (betrag_dy > betrag_dx) {
+      for (let x=x_start; x!=x_end; x=x+x_step) {
+          setPixel(y, x);
+          if (Q < 0){
+              Q=Q+Q_equal;
+
+          } else {
+              Q=Q+Q_step;
+              y=y+y_step;
+          }
+
+          console.log(Q);
       }
-  }
+    } else {
+      for (let x=x_start; x!=x_end; x=x+x_step) {
+          setPixel(x, y);
+          if (Q < 0){
+              Q=Q+Q_equal;
+
+          } else {
+              Q=Q+Q_step;
+              y=y+y_step;
+          }
+
+          console.log(Q);
+      }
+    }
 
 }
